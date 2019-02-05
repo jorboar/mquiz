9fc09c7e3123af05d96ebe9ddf6ac4702dda35a4
Jordan Perkic <jmaperkic@yahoo.com>
2019-02-05 03:38:59 +0000

    added scale/mode quiz

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	root 'static_pages#index'
 	get 'interval', to: 'static_pages#interval'
 	get 'chord', to: 'static_pages#chord'
+	get 'scale', to: 'static_pages#scale'
 
 	resources :questions
 	resources :quizs
