c14c9cb11647f737a8310cfdff90b05cf2fe5bbe
jorboar <jmaperkic@yahoo.com>
2019-04-03 21:32:51 +0000

    added database seed for boardNodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	i = 0 
+	while i < 31
+		BoardNode.create(quiz_id: 0, score: 0, name: "---")
+		i = i + 1;
+	end
